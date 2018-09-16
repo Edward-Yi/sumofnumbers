3012bdb0e0a813c35bb63dc58480f9c29ab3c91c
Edward Yi <42788217+Edward-Yi@users.noreply.github.com>
2018-09-16 12:52:39 -1000

    Code

---
+++ sumofnumbers.js
@@ -0,0 +1,37 @@
+const test = [1, 2, 3];
+
+function sumFor(num) {
+  let total = 0;
+  for (let i = 0; i < num.length; i++) {
+    total += num[i];
+  }
+  return total;
+}
+console.log(sumFor(test));
+
+function sumWhile(num) {
+  let total = 0;
+  let i = 0;
+  while (i < num.length) {
+    total += num[i];
+    i++;
+  }
+  return total;
+}
+console.log(sumWhile(test));
+
+function sumRecursion(num) {
+  if (num.length === 0) {
+    return 0;
+  }
+  const first = num.shift;
+  return first + sumRecursion(num);
+}
+console.log(sumRecursion(test));
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) {
+    return memo + num;
+  }, 0);
+}
+console.log(sumTheSimpleWay(test));
